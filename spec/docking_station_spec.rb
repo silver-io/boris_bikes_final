da95614699fa4b0c44a56a2d6074dbbd30de0ff2
javier <js@silveriofunk.com>
2014-08-30 23:12:58 +0100

    work continues on the docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
 
+	def fill_station(station)
+		10.times { station.dock(Bike.new) }
+	end
+
 	let(:bike)     { Bike.new }
 	let(:station)  { DockingStation.new(:capacity => 10) }
 
@@ -19,7 +23,12 @@ describe DockingStation do
 
 	it "should know when it's full" do
 		expect(station).not_to be_full
-		10.times { station.dock(Bike.new) }
+		fill_station station
 		expect(station).to be_full
+	end
+
+	it "should not accept a bike if full" do
+		fill_station station
+		expect(lambda {station.dock(bike) }).to raise_error(RuntimeError)
 	end
 end
